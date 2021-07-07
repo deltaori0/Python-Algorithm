8f1fafcdd5c3a7ea4304a3a7fa7e846e157019c8
Eunsoo <deltaori0@korea.ac.kr>
2021-07-07 09:53:29 +0900

    라이브러리 없이 조합 구현

---
+++ SW Expert Academy/완전 검색/조합.py
@@ -0,0 +1,20 @@
+def combination(arr, r):
+    arr = sorted(arr)   # 출력을 예쁘게 만들기 위해서 (선택)
+
+    def generate(chosen):
+        # chosen에 저장된 아이템 개수가 r개이면 조합이 하나 완성된 것 -> 출력 후 종료
+        if len(chosen) == r:
+            print(chosen)
+            return
+        
+        # start는 chosen이 비어있을 때는 0
+        start = arr.index(chosen[-1]) + 1 if chosen else 0
+        # chosen에 저장된 마지막 값 다음을 넣는다
+        for nxt in range(start, len(arr)):
+            chosen.append(arr[nxt])
+            generate(chosen)
+            chosen.pop()
+    
+    generate([])
+
+combination('ABCDE', 2)
